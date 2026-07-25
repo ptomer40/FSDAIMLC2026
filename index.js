7cf3b13a5b4906da2261b41cb9d48babd16cdee7
ptomer40 <ptomer40@yahoo.com>
2026-07-25 15:58:57 +0530

    callback function

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-function sum(a,b){
-    return a+b;
+// function sum(a,b){
+//     return a+b;
+// }
+
+// function sumWithMsg(clbk,msg){
+//            const result=clbk(20,40);
+//            console.log(msg+":"+result)
+// }
+
+// sumWithMsg(sum,"Hey using calculation with jS")
+
+
+
+
+function login(msg,error){
+    if(error){
+        console.log("Error is"+error)
+    }
+    else{
+        console.log("Welcome"+msg);
+    }
 }
-const result=sum(20,50);
-console.log("Sum="+result);
+
+ function loginVerification(username,password,clbk){
+   if(username=="ptomer40" && password=="1234556"){
+    clbk("Success",null);
+   }else{
+    clbk(null,"Username or password is incorrect")
+   }
+
+ }
+
+ loginVerification("ptomer40","1234556",login)
