0c2a488de72dbc4ec73872855d472676fc51286b
ptomer40 <ptomer40@yahoo.com>
2026-09-10 09:11:48 +0530

    react vite

---
+++ reactvite/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+import ICard from './component/ICard'
+
+function App() {
+  
+
+  return (
+    <div style={{border:'2px solid black'}}>
+    
+        <ICard />
+     
+    </div>
+  )
+}
+
+export default App
---
+++ reactvite/src/component/ICard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function ICard() {
+    let a=23;
+  let name="Ram"
+  return (
+    <div>
+        <h2 style={{backgroundColor:'brown',color:'white'}}>ABES ICard Component</h2>
+
+      <h2 style={{color:'red'}}>Welcome to React using Vite</h2>
+      <h2 style={{color:'red'}}>Value of a={a}</h2>
+      <h2 style={{color:'red'}}>Name={name}</h2>
+
+    </div>
+  )
+}
+
+export default ICard
